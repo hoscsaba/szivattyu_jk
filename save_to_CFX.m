6df90f8be9d334fb880538ef7f1d6be99245f44e
Márton Németh <nmartonb@gmail.com>
2024-02-09 17:42:17 +0100

    Kimentés CFX fájlformátumba

--- save_to_CFX.m
+++ save_to_CFX.m
@@ -1,15 +1,60 @@
 function save_to_CFD(geo)
-  fp=fopen("jk_A.txt","w");
-  w=0.1;
-  figure(1)
-  for i=1:length(geo.x_g(:,1))
-    r=sqrt(geo.x_g(i,1)^2+geo.y_g(i,1)^2);
-    theta=atan(geo.y_g(i,1)/geo.x_g(i,1));
-    if theta<0
-      theta=theta+pi;
+
+    fp=fopen("jk_A.rtzt", "w"); % Mentés Meanline Data File Format-ba
+
+    w=0.5; % lapátvastagság
+    LEmeshExt = 0.5; % Háló hosszabításának aránya radiális irányban a belépő élnél
+    TEmeshExt = 0.2; % Háló hosszabításának aránya radiális irányban a kilépő élnél
+    slices = 5; % rétegek száma b2 hosszon
+    
+    fprintf(fp,"%d 0",geo.N_lapat); % Lapátszám; "number of splitters"
+    fprintf(fp,"\n");
+    fprintf(fp,"\n 0.00 %d N M", slices); % "pitch fraction"; rétegek száma b2 hosszon; N-lapátvastagság normális irányba; M-?
+    
+    LEr2 = sqrt(geo.x_g(1,1)^2+geo.y_g(1,1)^2); % Belépő él sugara
+    LEr1 = LEr2 * LEmeshExt; % Háló legkisebb sugara
+
+    TEr1 = sqrt(geo.x_g(end,1)^2+geo.y_g(end,1)^2); % Kilépő él sugara
+    TEr2 = TEr1 * (1+TEmeshExt); % Háló legnagyobb sugara
+    
+    radNum = 5; % Hálóhosszabbítás felbontása
+    LErads = linspace(LEr1,LEr2,radNum+1);
+    TErads = linspace(TEr1,TEr2,radNum+1);
+
+    LEtheta = acos(geo.x_g(1,1)/LEr2); % Theta szög a belépő élnél
+    TEtheta = acos(geo.x_g(end,1)/TEr1); % Theta szög a kilépő élnél
+
+    for i = 1:slices
+        % Réteg magassága; "z" koordináta
+        b = -1000 * geo.b2 / (slices-1) * (i-1);
+        
+        % "span fraction"-rétegek felosztása 0.0-tól 1.0-ig?;
+        % ;rétegben megadott pontog száma
+        fprintf( fp,"\n \n %.5f %d", 1.0/(slices-1) * (i-1), 2*radNum+length(geo.x_g(:,1)) );
+        
+        % Háló hosszabbított szakasza (lapátvastagság=0.0)
+        for j = 1:radNum
+            % r, theta, z, b
+            fprintf(fp,"\n \t \t %.5f %.5f %.5f 0.00000", 1000*LErads(j), LEtheta, b);
+        end
+        
+        % Lapát szakasza
+        for j = 1:length(geo.x_g(:,1))
+            r = sqrt(geo.x_g(j,1)^2+geo.y_g(j,1)^2);
+            theta = acos(geo.x_g(j,1)/r);
+
+            % r, theta, z, b
+            fprintf(fp,"\n \t \t %.5f %.5f %.5f %.5f", 1000*r, theta, b, w);
+            plot(r, theta, '*'), hold on
+        end
+        
+        % Háló hosszabbított szakasza (lapátvastagság=0.0)
+        for j = 1:radNum
+            % r, theta, z, b
+            fprintf(fp, "\n \t \t %.5f %.5f %.5f 0.00000", 1000*TErads(j+1), TEtheta, b);
+        end
+
     end
-    fprintf(fp,"\n %5.3e %5.3e %5.3e %5.3e",r*1000,theta,geo.b2*1000,w);
-  plot(r,theta,'*'), hold on
-  end
-  fclose(fp);
+    
+    fclose(fp);
 end
